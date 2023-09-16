4edb13b2bb4f1a5d8d238ea2e0c00c6aca5e3461
apuntesnavarrete <apuntesnavarrete@gmail.com>
2023-09-15 23:19:25 -0600

    jugadores campeones json

--- src/routes/campeones.ts
+++ src/routes/campeones.ts
@@ -14,6 +14,14 @@ router.get('/api/campeones', async function(_req: Request, res: Response) {
   res.send(vista)
 });
 
+
+router.get('/api/campeones/jugadores', async function(_req: Request, res: Response) {
+  let consulta = "SELECT * FROM Jugadores_campeones_view"
+ let vista = await pool.query(consulta)
+  res.send(vista)
+});
+
+
 router.get('/api/campeones/:id', async (req: Request, res: Response) => {
   // Obtención del id del campeón
   //pamars recibe como string y hay que convertirlo en number.
@@ -38,6 +46,35 @@ router.get('/api/campeones/:id', async (req: Request, res: Response) => {
     res.status(404).send('No se encontró el campeón');
   }
 });
+
+
+router.get('/api/campeones/jugadores/:id', async (req: Request, res: Response) => {
+  // Obtención del id del campeón
+  //pamars recibe como string y hay que convertirlo en number.
+  const id: number = parseInt(req.params.id, 10);
+
+  // Validación del id del campeón
+  if (!Number.isInteger(id) || id < 1) {
+    res.status(400).send('El id del campeónato debe ser un número entero mayor que 0');
+    return;
+  }
+
+  // Consulta SQL para buscar el campeón
+  const consulta: string = `SELECT * FROM Jugadores_campeones_view WHERE Id_campeonato = ${id}`;
+
+  // Ejecución de la consulta
+  const resultados: any[] = await pool.query(consulta);
+
+
+  // Envío de la respuesta
+  if (resultados.length > 0) {
+    res.json(resultados);
+  } else {
+    res.status(404).send('No se encontró el campeón');
+  }
+});
+
+  
 
   
 
