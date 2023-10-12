b486e1c67e82b032b925554a69cdafb42e1f624d
apuntesnavarrete <apuntesnavarrete@gmail.com>
2023-10-12 16:28:28 -0600

    resulatados torneo dinamico desde sql

--- src/routes/Jornadas.ts
+++ src/routes/Jornadas.ts
@@ -17,16 +17,32 @@ router.get('/api/jornadas/:liga/:categoria', async (req: Request, res: Response)
   
    // console.log(id)
     // Validación 
-  
-    const liga = req.params.liga
     const categoria = req.params.categoria
-    const torneo_actual = "c23"
+    const liga = req.params.liga
+    
 
+    const consulta_filter: string = `SELECT * FROM tournaments_leagues_view WHERE shortened_brand_name = '${liga}' AND category = '${categoria}';`
+
+
+    const resultados_filter: any[] = await pool.query(consulta_filter).catch((error: Error) => {
+        if (error.message.includes("ER_NO_SUCH_TABLE")) {
+            // La tabla no existe
+            res.status(404).send('La tabla no existe');
+        } else {
+            // Otro error
+            res.status(500).send('error inesperado , revise su direccion');
+        }
+    });
+
+
+    const tournamentNames = resultados_filter.map((tournament) => tournament.name_tournament);
+
+
+   const torneo_actual = tournamentNames[tournamentNames.length-1]
   
     // Consulta SQL para buscar el campeón
     const consulta: string = "SELECT * FROM `futbolce_zon58`.`" + liga + "_jor_" + categoria + "_" + torneo_actual +"` LIMIT 1000";
-  
-console.log(consulta)
+
 
 
 const resultados: any[] = await pool.query(consulta).catch((error: Error) => {
